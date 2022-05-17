629c9692c0aeb9ff6ec4721540fd6eb544a80f93
n-shenoy <51956619+n-shenoy@users.noreply.github.com>
2022-05-17 18:46:33 +0530

    fixed mistakes

--- protein_to_mRNA.py
+++ protein_to_mRNA.py
@@ -5,47 +5,44 @@
 
 __author__ = "Navami Shenoy"
 
+codon_table = {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+ 
+amino_acids = {} # {'amino acid': frequency}
+# count how many codons code for 
+# the same amino acid 
+for acid in codon_table.values():
+    if acid not in amino_acids:
+        amino_acids[acid] = 1
+    else:
+        amino_acids[acid] += 1
+
+#print(amino_acids)
 
 def protein_to_mRNA(protein):
-    # table
-    amino_acids = { 'A': , 
-                    'R': ,
-                    'N': ,
-                    'D': ,
-                    'C': ,
-                    'Q': ,
-                    'E': ,
-                    'G': ,
-                    'H': ,
-                    'I': ,
-                    'L': ,
-                    'K': , 
-                    'M': , 
-                    'F': ,
-                    'P': ,
-                    'S': ,
-                    'T': ,
-                    'W': ,
-                    'Y': ,
-                    'V': 
-                }
-
-
-def readFASTA(filename):
-    """ read and parse FASTA file. capture 
-        labels and their corresponding sequences
-        in a dictionary """ 
-    # works more like parsing a FASTQ file
-    seqs = {}
-    with open(filename, 'r') as f:    
-        for line in f:
-            if line[0] == '>': # capture label
-                label = line[1:].rstrip()
-                genome = ''
-            else:    # capture sequence
-                genome += line.rstrip() 
-                seqs[label] = genome
-    return seqs
-
+    num_mrna = amino_acids['Stop']
+    for acid in protein:
+        num_mrna = (num_mrna*amino_acids[acid]) % 1000000
+    return num_mrna
 
 # testing
+test_file = open('rosalind_mrna.txt') # source: https://rosalind.info
+protein = test_file.read().rstrip()
+test_file.close()
+print(protein_to_mRNA(protein)) 
